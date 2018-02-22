ad9bd3f5c9c05ec304829e99c66ba3ba48d952fd
Blue <azhanging@qq.com>
2018-02-22 12:37:44 +0800

    update blue-tmpl prototype $ctx

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const path = require('path');
 const ROOT_PATH = process.cwd();
 
 module.exports = (opts) => {
-  const _opts = Object.assign(opts, {
+  const _opts = Object.assign({
     ext: 'tmpl'
-  });
+  }, opts);
   //配置express模板
-  if (opts.app) {
+  if ( opts.app ) {
     opts.app.engine(_opts.ext, (filePath, state, callback) => {
       fs.readFile(filePath, 'utf8', function (err, content) {
-        if (err) return callback(new Error(err));
+        if ( err ) return callback(new Error(err));
         return callback(null, new BlueTmpl({
           template: content,
         }).render(state).template);
@@ -23,19 +23,19 @@ module.exports = (opts) => {
   } else {
     //配置koa2的模板
     return function (ctx, next) {
-      if (ctx.render) return next();
+      if ( ctx.render ) return next();
       ctx.app.context.render = function (_path = "", data = {}) {
         return new Promise((resolve) => {
           fs.readFile(path.join(ROOT_PATH, opts.path, _path + `.${_opts.ext}`), 'utf8', (err, content) => {
-            if (err) {
+            if ( err ) {
               this.body = err;
             } else {
-              try{
+              try {
                 BlueTmpl.prototype.$ctx = this;
                 this.body = new BlueTmpl({
                   template: content,
                 }).render(data).template;
-              }catch(e){
+              } catch (e) {
                 this.body = e;
               }
             }
